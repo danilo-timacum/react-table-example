29e22dd5ccf151398ad332b974e09a249da527cb
danilo-timacum <danilo@timacum.com>
2021-12-05 21:45:48 +0100

    pagination in progress

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,7 @@
-import { useTable } from 'react-table'
+import { useState, useEffect } from 'react';
+import { useTable, usePagination } from 'react-table'
 
-const Table = ({ columns, data }) => {
+const Table = ({ columns, data, loading, pageSize, pageIndex1, setPageIndex, setPageFetchSize }) => {
 
     const {
         getTableProps,
@@ -8,15 +9,40 @@ const Table = ({ columns, data }) => {
         headerGroups,
         rows,
         prepareRow,
+        // for pagination START
+        page,
+        canPreviousPage,
+        canNextPage,
+        pageOptions,
+        pageCount,
+        gotoPage,
+        nextPage,
+        previousPage,
+        setPageSize,
+        // for pagination END
+        // Get the state from the instance
+        state: { pageIndex },
     } = useTable({
         columns,
         data,
-    })
+        initialState: { pageIndex: pageIndex1 }, // Pass our hoisted table state
+        manualPagination: true, // Tell the usePagination
+        // hook that we'll handle our own data fetching
+        // This means we'll also have to provide our own
+        // pageCount.
+        pageCount: 10,
+    },
+        usePagination
+    )
+
+    useEffect(() => {
+        setPageIndex(pageIndex)
+        console.log({pageIndex})
+    }, [pageIndex]);
 
     return (
 
         <>
-
             <table {...getTableProps()}>
                 <thead>
                     {headerGroups.map(headerGroup => (
@@ -27,7 +53,8 @@ const Table = ({ columns, data }) => {
                         </tr>
                     ))}
                 </thead>
-                <tbody {...getTableBodyProps()}>
+
+                {/* <tbody {...getTableBodyProps()}>
                     {rows.map((row, i) => {
                         prepareRow(row)
                         return (
@@ -38,8 +65,84 @@ const Table = ({ columns, data }) => {
                             </tr>
                         )
                     })}
+                </tbody> */}
+
+                <tbody {...getTableBodyProps()}>
+                    {page.map((row, i) => {
+                        prepareRow(row)
+                        return (
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map(cell => {
+                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                })}
+                            </tr>
+                        )
+                    })}
+                    <tr>
+                        {loading ? (
+                            // Use our custom loading state to show a loading indicator
+                            <td colSpan="10000">Loading...</td>
+                        ) : (
+                            <td colSpan="10000">
+                                {/* Showing {page.length} of ~{controlledPageCount * pageSize}{' '}
+                                results */}
+                            </td>
+                        )}
+                    </tr>
                 </tbody>
+
+
             </table>
+
+            {/* 
+        Pagination can be built however you'd like. 
+        This is just a very basic UI implementation:
+      */}
+            <div className="pagination">
+                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </button>{' '}
+                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </button>{' '}
+                <button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </button>{' '}
+                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={10} // page size
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </div>
+
         </>
 
     );
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,21 +1,39 @@
+
+import React, { useState, useEffect } from 'react';
 import { useFetchData } from "../hooks/useFetchData";
 import Table from "../components/Table";
 import { UseColumns } from "../components/Columns";
 
 const TablePage = () => {
-
-    const {data: photos, isPending, error} = useFetchData(0, 10);
+    
+    const [pageIndex, setPageIndex] = useState(1);
+    const [pageSize, setPageFetchSize] = useState(10);
+    const { data: photos, isPending, error } = useFetchData(pageIndex, pageSize);
     const { columns } = UseColumns();
-    console.log({columns})
+    console.log({ columns })
 
     console.log(photos);
+
+    // useEffect(() => {
+    //     fetchData({ pageIndex, pageSize })
+    // }, [fetchData, pageIndex, pageSize])
 
     return (
         <div>
             {isPending ? 'loading...' : ''}
             {error}
             dftgds
-            { !isPending && photos && columns ? <Table columns={columns} data={photos}/> : '' }
+            {!isPending && photos && columns ?
+                <Table 
+                    columns={columns} 
+                    data={photos} 
+                    loading={isPending}
+                    pageIndex1={pageIndex}
+                    pageSize={pageSize}
+                    setPageIndex={setPageIndex}
+                    setPageFetchSize={setPageFetchSize}
+                />
+                : ''}
         </div>
     );
 }
